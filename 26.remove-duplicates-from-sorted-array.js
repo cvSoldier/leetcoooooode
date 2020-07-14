b75b256bb9889362f9bd2515cef9947f078f5673
yanchao.wang <yanchao.wang@meritco-group.com>
2020-07-14 17:24:32 +0800

    *26

--- 26.remove-duplicates-from-sorted-array.js
+++ 26.remove-duplicates-from-sorted-array.js
@@ -13,15 +13,17 @@ var removeDuplicates = function(nums) {
   let begin = iter = delLen = 0
   for(let len = nums.length; iter < len + 1; iter++) {
     if(nums[iter] !== nums[begin]) {
-      delLen++
-      begin = iter
+      delLen = iter - begin - 1
+      delLen && nums.splice(begin, delLen)
+      begin++
+      iter = begin
+      len -= delLen
     }
   }
-  return delLen
 };
-// var arr = [0,0,1,1,1,2,2,3,3,4,4,4]
-var arr = [1,1,3]
-// removeDuplicates(arr)
-console.log(arr, removeDuplicates(arr));
+var arr = [0,0,1,1,1,2,2,3,3,4,4,4]
+// var arr = [1,2,3]
+removeDuplicates(arr)
+console.log(arr);
 // @lc code=end
 
