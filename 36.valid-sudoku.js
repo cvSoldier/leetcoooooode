962d31e812c517d498ccddc880efd994d58e5b37
cvSoldier <610496306@qq.com>
2020-08-10 22:58:41 +0800

    *36

--- 36.valid-sudoku.js
+++ 36.valid-sudoku.js
@@ -87,45 +87,76 @@
  * @param {character[][]} board
  * @return {boolean}
  */
+// var isValidSudoku = function(board) {
+//   function hasRepeatNum(nums) {
+//     var map = {}
+//     for(let i = 0; i < board.length; i++) {
+//       if(map[nums[i]] && nums[i] !== '.') return true
+//       map[nums[i]] = true
+//     }
+//     return false
+//   }
+//   let isValid = false
+
+//   for(let i = 0; i < board.length; i++) {
+//     isValid = isValid || hasRepeatNum(board[i])
+//   }
+//   var arr = []
+//   for(let i = 0; i < board.length; i++) {
+//     if(!isValid) {
+//       for(let j = 0; j < board.length; j++) {
+//         arr.push(board[j][i])
+//       }
+//       isValid = hasRepeatNum(arr)
+//     }
+//     arr.length = 0
+//   }
+//   for(let i = 0; i < board.length; i += 3) {
+//     for(let j = 0; j < board.length; j += 3) {
+//       if(!isValid) {
+//         arr = [
+//           board[i][j], board[i][j + 1], board[i][j + 2],
+//           board[i + 1][j], board[i + 1][j + 1], board[i + 1][j + 2],
+//           board[i + 2][j], board[i + 2][j + 1], board[i + 2][j + 2],
+//         ]
+//         isValid = hasRepeatNum(arr)
+//         arr.length = 0
+//       }
+//     }
+//   }
+//   return !isValid
+// };
 var isValidSudoku = function(board) {
-  function hasRepeatNum(nums) {
-    var map = {}
-    for(let i = 0; i < board.length; i++) {
-      if(map[nums[i]] && nums[i] !== '.') return true
-      map[nums[i]] = true
+  return backTrack(board, 0, 0)
+};
+function backTrack(board, i, j) {
+  if(j > 8) { i++; j = 0 }
+  if(i > 8) return true
+
+  if(board[i][j] !== '.') {
+    if(validPartial(board, i, j, board[i][j])) {
+      return backTrack(board, i, j + 1)
     }
     return false
+  } else {
+    return backTrack(board, i, j + 1)
   }
-  let isValid = false
-
-  for(let i = 0; i < board.length; i++) {
-    isValid = isValid || hasRepeatNum(board[i])
+}
+function validPartial(board, i, j, c) {
+  for(let x = 0; x < 9; x++) {
+    if(board[i][x] === c && x !== j) return false
   }
-  var arr = []
-  for(let i = 0; i < board.length; i++) {
-    if(!isValid) {
-      for(let j = 0; j < board.length; j++) {
-        arr.push(board[j][i])
-      }
-      isValid = hasRepeatNum(arr)
-    }
-    arr.length = 0
+  for(let y = 0; y < 9; y++) {
+    if(board[y][j] === c && y !== i) return false
   }
-  for(let i = 0; i < board.length; i += 3) {
-    for(let j = 0; j < board.length; j += 3) {
-      if(!isValid) {
-        arr = [
-          board[i][j], board[i][j + 1], board[i][j + 2],
-          board[i + 1][j], board[i + 1][j + 1], board[i + 1][j + 2],
-          board[i + 2][j], board[i + 2][j + 1], board[i + 2][j + 2],
-        ]
-        isValid = hasRepeatNum(arr)
-        arr.length = 0
-      }
+  var row = i - i % 3, col = j - j % 3
+  for(let x = 0; x < 3; x++) {
+    for(let y = 0; y < 3; y++) {
+      if(board[row + x][col + y] === c && (row + x !== i && col + y !== j)) return false
     }
   }
-  return !isValid
-};
+  return true
+}
 // console.log(isValidSudoku(
 //   [
 //     ["8","3",".",".","7",".",".",".","."],
