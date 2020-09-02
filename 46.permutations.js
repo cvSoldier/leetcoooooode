d1f2bf6bc556b1ea37b21af32500156ce17e833f
cvSoldier <610496306@qq.com>
2020-09-02 11:53:15 +0800

    +46

---
+++ 46.permutations.js
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode id=46 lang=javascript
+ *
+ * [46] Permutations
+ *
+ * https://leetcode.com/problems/permutations/description/
+ *
+ * algorithms
+ * Medium (63.13%)
+ * Likes:    4158
+ * Dislikes: 108
+ * Total Accepted:    634.7K
+ * Total Submissions: 997K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a collection of distinct integers, return all possible permutations.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,2,3]
+ * Output:
+ * [
+ * ⁠ [1,2,3],
+ * ⁠ [1,3,2],
+ * ⁠ [2,1,3],
+ * ⁠ [2,3,1],
+ * ⁠ [3,1,2],
+ * ⁠ [3,2,1]
+ * ]
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+  var result = []
+  backtrack(nums, 0, [], result)
+  return result
+};
+var backtrack = function(nums, n, cur, result) {
+  // debugger
+  if(n === nums.length) {
+    result.push([...cur])
+    return
+  }
+
+  var len = nums.length
+  while(--len >= 0) {
+    if(!cur.includes(nums[len])) {
+      cur.push(nums[len])
+      backtrack(nums, n + 1, cur, result)
+      cur.pop()
+    }
+  }
+}
+// console.log(...permute([1,2,3]));
+// @lc code=end
+
