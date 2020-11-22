22e83c3bfd3cba9f3c13a22a6596ea6d62eacf62
cvSoldier <610496306@qq.com>
2020-11-22 22:30:15 +0800

    补充题目url

--- cn-contest/216/5606.js
+++ cn-contest/216/5606.js
@@ -1,3 +1,5 @@
+// https://leetcode-cn.com/problems/smallest-string-with-a-given-numeric-value/
+
 var getSmallestString = function(n, k) {
   const map = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
               'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
--- cn-contest/216/5607.js
+++ cn-contest/216/5607.js
@@ -1,3 +1,5 @@
+// https://leetcode-cn.com/problems/ways-to-make-a-fair-array/
+
 /**
  * @param {number[]} nums
  * @return {number}
