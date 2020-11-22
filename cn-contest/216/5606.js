7089ca93d6183f93fbb4e6a61b674b5275950579
cvSoldier <610496306@qq.com>
2020-11-22 22:26:29 +0800

    周赛216

---
+++ cn-contest/216/5606.js
@@ -0,0 +1,46 @@
+var getSmallestString = function(n, k) {
+  const map = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+              'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  // let aNum = 0
+  // while(true) {
+  //   debugger
+  //   let temp = (k / (27 / 2))
+  //   if(temp <= n || n == 1 || (k - 27 < 26 * (n - 2)) ) break
+  //   k = k - 27
+  //   n -= 2
+  //   aNum++
+  // }
+  // let result = ''
+  // for(let i = 0; i < aNum; ++i) {
+  //   result += 'a'
+  // }
+  // let centerNum = n
+  // while(centerNum) {
+  //   if(centerNum != 1) {
+  //     result += 'a'
+  //     k--
+  //   } else {
+  //     result += map[k - 1]
+  //   }
+  //   centerNum--
+  // }
+  // for(let i = 0; i < aNum; ++i) {
+  //   result += 'z'
+  // }
+  // return result
+  let result = new Array(n).fill('')
+  for(let len = n - 1; len >= 0; --len) {
+    if(k - (len) >= 26) {
+      result[len] = 'z'
+      k -= 26
+    } else {
+      // debugger
+      result[len] = map[k - len - 1]
+      k -= (k - len)
+    }
+  }
+  return result.join('')
+};
+console.log(getSmallestString(4, 100));
+console.log(getSmallestString(5, 73));
+console.log(getSmallestString(3, 27));
---
+++ cn-contest/216/5607.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var waysToMakeFair = function(nums) {
+  let oddTotal = evenTotal = total = 0
+  let even = true
+  for(let i = 0; i < nums.length; ++i) {
+    if(even) evenTotal += nums[i]
+    else oddTotal += nums[i]
+    even = !even
+  }
+  total = evenTotal + oddTotal
+  
+  even = true
+  let result = 0
+  let oddZeroToI = evenZeroToI = 0
+  let oddIToN = oddTotal
+  let evenIToN = evenTotal
+  for(let i = 0; i < nums.length; ++i) {
+    debugger
+    if(even) {
+      evenIToN -= nums[i]
+      if(oddZeroToI + evenIToN === evenZeroToI + oddIToN) {
+        result++
+      }
+      evenZeroToI += nums[i]
+    } else {
+      oddIToN -= nums[i]
+      if(oddZeroToI + evenIToN === evenZeroToI + oddIToN) {
+        result++
+      }
+      oddZeroToI += nums[i]
+    }
+    even = !even
+  }
+  return result
+};
+console.log(waysToMakeFair([2,1,6,4]));
