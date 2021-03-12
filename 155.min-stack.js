d3caa75bcd0600c0683e41a458672b5b7157478f
cvSoldier <610496306@qq.com>
2021-03-12 17:47:16 +0800

    +155 击败5.12% 没吃过被门夹过的核桃都写不出这种代码

---
+++ 155.min-stack.js
@@ -0,0 +1,109 @@
+/*
+ * @lc app=leetcode id=155 lang=javascript
+ *
+ * [155] Min Stack
+ *
+ * https://leetcode.com/problems/min-stack/description/
+ *
+ * algorithms
+ * Easy (44.82%)
+ * Likes:    4715
+ * Dislikes: 436
+ * Total Accepted:    689.2K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n' +
+  '[[],[-2],[0],[-3],[],[],[],[]]'
+ *
+ * Design a stack that supports push, pop, top, and retrieving the minimum
+ * element in constant time.
+ * 
+ * 
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * getMin() -- Retrieve the minimum element in the stack.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input
+ * ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ * [[],[-2],[0],[-3],[],[],[],[]]
+ * 
+ * Output
+ * [null,null,null,null,-3,null,0,-2]
+ * 
+ * Explanation
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin(); // return -3
+ * minStack.pop();
+ * minStack.top();    // return 0
+ * minStack.getMin(); // return -2
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * Methods pop, top and getMin operations will always be called on non-empty
+ * stacks.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function() {
+  this.stack = []
+  this.originStack = []
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+  this.originStack.push(x)
+  this.stack.push(x)
+  this.stack.sort((a,b) => b - a)
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+  this.originStack.pop()
+  this.stack = this.originStack.slice(0).sort((a,b) => b - a)
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+  return this.originStack[this.originStack.length - 1]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+  return this.stack[this.stack.length - 1]
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+// @lc code=end
+
