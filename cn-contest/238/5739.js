69b03a521200717b8af2b35ea8a6dfe3c71e8519
cvSoldier <610496306@qq.com>
2021-04-25 16:09:12 +0800

    周赛238 5739这题竟然是滑动窗口 绝了

---
+++ cn-contest/238/5739.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxFrequency = function(nums, k) {
+  nums.sort((a, b) => a - b)
+  let i = 0;
+  let j = 1;
+  let sum = 0
+  let ans = 0
+  while(j < nums.length) {
+    sum += (nums[j] - nums[i]) * (j - i)
+    while(sum > k) {
+      sum -= (nums[j] - nums[i])
+    }
+    ans = Math.max(ans, j - i + 1)
+  }
+  return ans
+};
+// 这题竟然是滑动窗口，绝了
