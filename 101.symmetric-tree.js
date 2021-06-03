25a7d24d06ba8723e3c1e9f6df9dca014498d246
cvSoldier <610496306@qq.com>
2021-06-03 18:02:20 +0800

    +101 中等dfs

---
+++ 101.symmetric-tree.js
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode id=101 lang=javascript
+ *
+ * [101] Symmetric Tree
+ *
+ * https://leetcode.com/problems/symmetric-tree/description/
+ *
+ * algorithms
+ * Easy (47.07%)
+ * Likes:    6316
+ * Dislikes: 167
+ * Total Accepted:    904.8K
+ * Total Submissions: 1.9M
+ * Testcase Example:  '[1,2,2,3,4,4,3]'
+ *
+ * Given the root of a binary tree, check whether it is a mirror of itself
+ * (i.e., symmetric around its center).
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [1,2,2,3,4,4,3]
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [1,2,2,null,3,null,3]
+ * Output: false
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [1, 1000].
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ * 
+ * Follow up: Could you solve it both recursively and iteratively?
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+  function equal(node1, node2) {
+    if(node1 === null && node2 === null) return true
+    if(node1 === null || node2 === null) return false
+    return (node1.val === node2.val) && equal(node1.left, node2.right) && equal(node2.left, node1.right)
+  }
+  return equal(root, root)
+};
+
+// @lc code=end
+
