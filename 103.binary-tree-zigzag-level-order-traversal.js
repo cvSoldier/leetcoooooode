0cc23bde6d7e4a9a23bd804da1d447dd814e07ba
cvSoldier <610496306@qq.com>
2021-06-07 22:55:41 +0800

    +103 简单bfs

---
+++ 103.binary-tree-zigzag-level-order-traversal.js
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=103 lang=javascript
+ *
+ * [103] Binary Tree Zigzag Level Order Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (50.90%)
+ * Likes:    3585
+ * Dislikes: 130
+ * Total Accepted:    523.4K
+ * Total Submissions: 1M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given the root of a binary tree, return the zigzag level order traversal of
+ * its nodes' values. (i.e., from left to right, then right to left for the
+ * next level and alternate between).
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: [[3],[20,9],[15,7]]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [1]
+ * Output: [[1]]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = []
+ * Output: []
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 2000].
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function(root) {
+  if(!root) return []
+  let queue = [root]
+  const result = []
+  let bool = true
+  while (queue.length) {
+    const tempQ = []
+    const childRes = []
+    for(let i = 0; i < queue.length; ++i) {
+      const node = queue[i]
+      childRes.push(node.val)
+      node.left && tempQ.push(node.left)
+      node.right && tempQ.push(node.right)
+    }
+    result.push(bool ? childRes : childRes.reverse())
+    bool = !bool
+    queue = tempQ
+  }
+  return result
+};
+// @lc code=end
