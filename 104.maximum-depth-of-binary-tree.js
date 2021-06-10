82401632e0ceee1a84b4369f860909e96eeb84c0
cvSoldier <610496306@qq.com>
2021-06-10 23:57:24 +0800

    +104 简单bfs

---
+++ 104.maximum-depth-of-binary-tree.js
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode id=104 lang=javascript
+ *
+ * [104] Maximum Depth of Binary Tree
+ *
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (68.84%)
+ * Likes:    4106
+ * Dislikes: 98
+ * Total Accepted:    1.2M
+ * Total Submissions: 1.7M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given the root of a binary tree, return its maximum depth.
+ * 
+ * A binary tree's maximum depth is the number of nodes along the longest path
+ * from the root node down to the farthest leaf node.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: 3
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [1,null,2]
+ * Output: 2
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = []
+ * Output: 0
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: root = [0]
+ * Output: 1
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 10^4].
+ * -100 <= Node.val <= 100
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if(!root) return 0
+  let num = 0
+  let queue = [root]
+  while (queue.length) {
+    const tempQ = []
+    for(let i = 0; i < queue.length; ++i) {
+      const node = queue[i]
+      node.left && tempQ.push(node.left)
+      node.right && tempQ.push(node.right)
+    }
+    num++
+    queue = tempQ
+  }
+  return num
+};
+// @lc code=end
+
